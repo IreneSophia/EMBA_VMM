dace1e5553c237f2583c5b6f15be71f2f15d751c
IreneSophia <10planki@gmail.com>
2024-01-31 14:59:45 +0100

    Changed the RMarkdown to an R file for the preprocessing. Added in excluding participants based on false alarms.

--- preproBV_VMM.R
+++ preproBV_VMM.R
@@ -1,12 +1,3 @@
----
-title: "Analysis VMM: Behavioural Data"
-author: "Irene Sophia Plank"
-date: "`r Sys.Date()`"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
 library(knitr)           # kable
 library(tidyverse)       # tibble stuff
 library(ggplot2)         # plots
@@ -15,23 +6,10 @@ library(ggpubr)          # background image
 library(png)             # readPNG
 
 dt.path = paste('/home/emba/Documents/EMBA', 'BVET', sep = "/")
-knitr::opts_knit$set(root.dir = dt.path)
 
-```
+# participants from whom we can use the MRI data
+df.inc  = read_csv(file.path(dt.path, 'df_VMM_include.csv'))
 
-## R Markdown
-
-<style type="text/css">
-.main-container {
-  max-width: 1100px;
-  margin-left: auto;
-  margin-right: auto;
-}
-</style>
-
-# Behavioural data
-
-```{r load_data, warning=F, message=F}
 # load the relevant data in long format: only people with separate logs for runs
 df.log1 = list.files(path = dt.path, pattern = "*task-.*\\.tsv$", full.names = T) %>%
   setNames(nm = .) %>%
@@ -57,11 +35,27 @@ df.log2 = list.files(path = dt.path, pattern = "*task.tsv$", full.names = T) %>%
 # bind them together
 df.log = rbind(df.log1, df.log2)
 
+# merge with subjects
+df.log = merge(df.inc, df.log, all.x = T)
+
 # only rows relevant for answer
 df.logresp = df.log %>% 
   filter(Code != 255 & ((`Event Type` == "Response" & Code <= 4) | `flp_rel(num)` == 1)) %>%
   arrange(Subject, Run, Trial) %>%
   select(Subject, Run, Trial, `Event Type`, Time)
+
+# get trial numbers for the flips
+df.flips = df.log %>% 
+  filter((`Event Type` == "Picture" & `flp_rel(num)` == 1)) %>%
+  arrange(Subject, Run, Trial) %>%
+  group_by(Subject) %>%
+  mutate(
+    trl = row_number()
+  ) %>% ungroup() %>%
+  select(Subject, Run, Trial, trl)
+
+# merge responses with flips
+df.logresp = merge(df.logresp, df.flips, all = T)
 
 # find relevant responses to each flip
 sub  = c()
@@ -80,9 +74,9 @@ for (i in 1:nrow(df.logresp)) {
     } else {
       look  = "Picture"
     }
-  # if we are not in the first row, check if it's a new run or subject
+    # if we are not in the first row, check if it's a new run or subject
   } else if ((df.logresp$Subject[i] != df.logresp$Subject[i-1]) | 
-        (df.logresp$Run[i] != df.logresp$Run[i-1])) {
+             (df.logresp$Run[i] != df.logresp$Run[i-1])) {
     # check if it's a picture, otherwise set look to picture
     if (df.logresp$`Event Type`[i] == "Picture") {
       onset = df.logresp$Time[i] # ...get the onset and look for response
@@ -90,31 +84,31 @@ for (i in 1:nrow(df.logresp)) {
     } else {
       look  = "Picture"
     }
-  # if it's not a new run or subject and it's a picture and we're looking for a picture...
+    # if it's not a new run or subject and it's a picture and we're looking for a picture...
   } else if (df.logresp$`Event Type`[i] == look & look == "Picture") {
     onset = df.logresp$Time[i] # ...get the onset and look for response
     look  = "Response"
-  # if we are looking for a response and it is one...
+    # if we are looking for a response and it is one...
   } else if (df.logresp$`Event Type`[i] == look & look == "Response") {
     sub   = c(sub, df.logresp$Subject[i]) # ...log it all!
     run   = c(run, df.logresp$Run[i])
-    trl   = c(trl, df.logresp$Trial[i])
+    trl   = c(trl, df.logresp$trl[i-1])
     rt    = c(rt, (df.logresp$Time[i] - onset)/10)
     sdt   = c(sdt, "hit")
     look  = "Picture"
-  # if it's a picture and we are looking for a response
+    # if it's a picture and we are looking for a response
   } else if (df.logresp$`Event Type`[i] == "Picture" & look == "Response") {
     sub   = c(sub, df.logresp$Subject[i-1])
     run   = c(run, df.logresp$Run[i-1])
-    trl   = c(trl, df.logresp$Trial[i-1])
+    trl   = c(trl, df.logresp$trl[i-1])
     rt    = c(rt, NA)
     sdt   = c(sdt, "miss")
     onset = df.logresp$Time[i]
-  # if it's a response and we are looking for a picture
+    # if it's a response and we are looking for a picture
   } else if (df.logresp$`Event Type`[i] == "Response" & look == "Picture") {
     sub   = c(sub, df.logresp$Subject[i-1])
     run   = c(run, df.logresp$Run[i-1])
-    trl   = c(trl, df.logresp$Trial[i-1])
+    trl   = c(trl, NA)
     rt    = c(rt, NA)
     sdt   = c(sdt, "faal")
   }
@@ -131,45 +125,45 @@ df.tsk = data.frame(sub, run, trl, rt, sdt) %>%
     cl    = quantile(rt, probs = c(.75), na.rm = T) + 1.5 * iqr,
     fl    = quantile(rt, probs = c(.25), na.rm = T) - 1.5 * iqr,
     rtc   = ifelse(rt > cl | rt < fl, NA, rt),
-    sdt   = as.factor(case_when(rt <= 1500 ~ sdt,
-                                rt >  1500 ~ "miss", # answers slower than this are counted as misses
+    sdt   = as.factor(case_when(rt <= 2000 ~ sdt,
+                                rt >  2000 ~ "miss", # answers slower than this are counted as misses
                                 is.na(rt)  ~ sdt))
-    ) %>% select(-iqr, -cl, -fl)
+  ) %>% select(-iqr, -cl, -fl)
 
 rm(list = c("df.log1","df.log2"))
 
-```
-
-```{r plot_data, warning=F, message=F}
-
-# aggregate the data to see average reaction times and accuracies per run
-df.tsk_agg = df.tsk %>%
-  group_by(subID, run) %>%
-  summarise(
-    rt_agg = mean(rtc, na.rm = T),
-    miss   = sum(sdt == "miss")/120      # there are 120 flips per run
-  ) 
-
-# how many people need to be excluded for missing too many changes in a run?
-exc = df.tsk_agg %>% filter(miss > 1/3) %>% select(subID)
-exc = unique(as.character(exc$subID))
-print(length(exc))
-
-# load list of pilot participant to add them to the list to be excluded
-pilot = read_csv(paste0(dt.path, "/pilot-subIDs.csv"))
-exc   = c(exc, pilot$subID)
-
-# filter out from original data frame
+# remove participants who missed more than 1/3 of fixation cross changes per run
+# or more than 1/3 of false alarms in one of the runs
 df.tsk = df.tsk %>%
-  filter(!(subID %in% exc)) # disregard runs with too many misses
+  group_by(subID, run) %>%
+  mutate(
+    run.miss   = sum(sdt == "miss")/120,      # there are 120 flips per run
+    run.faal   = sum(sdt == "faal")/120
+  ) %>%
+  group_by(subID) %>%
+  mutate(
+    max.miss = max(run.miss),
+    max.faal = max(run.faal)
+  ) %>%
+  filter(max.miss < 0.33 & max.faal < 0.33) %>%
+  ungroup()
+
+nsub = length(unique(df.tsk$subID))
+
+# how many people were excluded due to behavioural data?
+nrow(df.inc) - nsub
+
+# create a data frame with discrimination rate
+df.disc = df.tsk %>%
+  group_by(subID) %>%
+  summarise(
+    hits = sum(sdt == "hit", na.rm = T),
+    faal = sum(sdt == "faal", na.rm = T),
+  ) %>%
+  mutate(
+    disc = hits - faal
+  )
 
 # save the data frame
-saveRDS(df.tsk, paste0(dt.path, "/df_VMM.RDS"))
+save(df.tsk, df.disc, file = file.path(dt.path, "VMM_data.RData"))
 
-# plot the reaction time distributions
-ggplot(df.tsk, aes(x = rtc, fill = subID)) + geom_density(alpha=.3) + theme_minimal() + theme(legend.position = "none") 
-
-# print the number of participants
-length(unique(df.tsk$subID))
-
-```
